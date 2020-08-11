09ca15717e4a40adb16870aedd0e10246f93bb46
kevjlope <kevinjeff04@gmail.com>
2020-08-11 02:32:26 -0500

    Se muestra la informacion del usuario cuando hace login, en el fragment profile

---
+++ app/src/main/java/com/example/oxygen/User.java
@@ -0,0 +1,12 @@
+package com.example.oxygen;
+
+public class User {
+    private String nombre;
+    private String correo;
+
+    public User(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+}
